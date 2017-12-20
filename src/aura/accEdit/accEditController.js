0736422ac4eaa73df84d0c83fa72730eff968a39
Chris Matos <cmatos@smbhd.com>
2017-12-20 17:49:51 -0600

    data service errors

--- src/aura/accEdit/accEditController.js
+++ src/aura/accEdit/accEditController.js
@@ -15,5 +15,28 @@
                     console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
                 }
             }));
-    }
+    },
+    // Control the component behavior here when record is changed (via any component)
+        handleRecordUpdated: function(component, event, helper) {
+            var eventParams = event.getParams();
+            console.log(eventParams);
+            if(eventParams.changeType === "CHANGED") {
+                // get the fields that are changed for this record
+                var changedFields = eventParams.changedFields;
+                console.log('Fields that are changed: ' + JSON.stringify(changedFields));
+                // record is changed so refresh the component (or other component logic)
+                var resultsToast = $A.get("e.force:showToast");
+                resultsToast.setParams({
+                    "title": "Saved",
+                    "message": "The record was updated."
+                });
+                resultsToast.fire();
+            } else if(eventParams.changeType === "LOADED") {
+                // record is loaded in the cache
+            } else if(eventParams.changeType === "REMOVED") {
+                // record is deleted and removed from the cache
+            } else if(eventParams.changeType === "ERROR") {
+                component.set("v.recordSaveError", component.get("v.error"));
+            }
+        }
 })
