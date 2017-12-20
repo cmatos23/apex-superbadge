0c9d7259a11826a5a66e104b5d0df13f43cd0929
Chris Matos <cmatos@smbhd.com>
2017-12-20 16:00:33 -0600

    event solution

--- src/aura/campingList/campingListController.js
+++ src/aura/campingList/campingListController.js
@@ -1,36 +1,42 @@
-/**
- * Created by ChristopherMatos on 12/18/2017.
- */
 ({
-    doInit : function(component, event){
-        var action = component.get("c.getItems");
-        action.setCallback(this, function(response){
-            var state = response.getState();
-            if (state === "SUCCESS") {
+    // Load items from Salesforce
+	doInit: function(component, event, helper) {
 
-                component.set("v.items", response.getReturnValue());
-            }
-            else{
-                console.error(response.getError()[0].message)
-            }
-        });
-        $A.enqueueAction(action);
-    },
-     clickCreateItem : function(component, event){
+    // Create the action
+    var action = component.get("c.getItems");
 
-         var validItem = component.find('campingform').reduce(function (validSoFar, inputCmp) {
-                    // Displays error messages for invalid fields
-                    inputCmp.showHelpMessageIfInvalid();
-                    return validSoFar && inputCmp.get('v.validity').valid;
-                }, true);
-        // If we pass error checking, do some real work
-        if(validItem){
-             var items = component.get("v.items");
-            items.push(newItem);
-            component.set("v.items", items);
-
-            component.set("v.newItem", {'sobjectType' : 'Camping_Item__c', 'Quantity__c' : 0, 'Price__c' : 0});
-
+    // Add callback behavior for when response is received
+    action.setCallback(this, function(response) {
+        var state = response.getState();
+        if (component.isValid() && state === "SUCCESS") {
+            component.set("v.items", response.getReturnValue());
         }
-    }
+        else {
+            console.log("Failed with state: " + state);
+        }
+    });
+
+    // Send action off to be executed
+    $A.enqueueAction(action);
+},
+
+
+    handleAddItem: function(component, event, helper) {
+    //   var newItem = event.getParam("item");
+    //helper.addItem(component, newItem);
+    var action = component.get("c.saveItem");
+    		action.setParams({"item": newItem});
+    		action.setCallback(this, function(response){
+        		var state = response.getState();
+        		if (component.isValid() && state === "SUCCESS") {
+            		// all good, nothing to do.
+            var items = component.get("v.items");
+            items.push(response.getReturnValue());
+            component.set("v.items", items);
+        		}
+    		});
+    		$A.enqueueAction(action);
+        		}
+
+
 })
