a9f57c7de2fa95d01b6884c6ec572b03e972ce12
Chris Matos <christopher.matos@ibm.com>
2018-01-09 09:32:38 -0600

    searching restuarant functionality

---
+++ src/aura/InTheArea/InTheAreaController.js
@@ -0,0 +1,11 @@
+({
+
+    doInit : function(component, event, helper) {
+        helper.getLocalList(component);
+    },
+    updateSearch: function (component, event, helper) {
+        helper.getLocalList(component);
+    }
+
+
+})
